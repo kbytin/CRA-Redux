9ed8de85cfbc45c28fef6e3232083c12a7d870dc
kbytin <kirill@bytin.net>
2018-02-06 15:23:37 +0200

    adding more reducers and action types to const

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
+import {updateCurrent} from './reducers/todo'
 
+const todoChangeHandler = (val) => store.dispatch(updateCurrent(val))
 
 const render = () => {
   const state = store.getState()
-  ReactDOM.render(<App {...state} />, document.getElementById('root'));
+  ReactDOM.render(<App
+    todos={state.todos}
+    currentTodo={state.currentTodo}
+    changeCurrent={todoChangeHandler}
+    />, document.getElementById('root'));
 }
 
 render()
