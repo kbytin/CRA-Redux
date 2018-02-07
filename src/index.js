b922bc1110069654d5134e4f18dc8b000ab1c2e9
kbytin <kirill@bytin.net>
2018-02-07 06:43:51 +0200

    Refactoring, simplifying

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Propvider, Provider} from 'react-redux'
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
